e920db74a882b9cb789dba6a6a1602c273fd48af
gerray <wyq0213@gmail.com>
2022-03-08 11:11:55 +0900

    benchmark追加

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeatd string
-	for i := 0; i < 5; i++ {
-		repeatd = repeatd + character
+	for i := 0; i < repeatCount; i++ {
+		repeatd += character
 	}
 	return repeatd
 }
