8c5dd3d3eda3887a441ae76c7fe0c5f7abe232b5
gerray <wyq0213@gmail.com>
2022-03-08 09:26:14 +0900

    挨拶受信者指定できるように修正

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,9 +2,15 @@ package main
 
 import "testing"
 
+/*
+テスト作成ルール
+1 xxx_test.goのような名前のファイルにある必要があります。
+2 テスト関数はTestという単語で始まる必要があります。
+3 テスト関数は1つの引数のみをとります。 t *testing.T
+*/
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q wang %q", got, want)
